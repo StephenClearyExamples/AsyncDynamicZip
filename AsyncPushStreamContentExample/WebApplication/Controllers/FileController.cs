003f5a1e6341a42ea6c79c9abca2f492d8ae6d17
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-10-14 18:47:48 -0400

    Zip download

--- AsyncPushStreamContentExample/WebApplication/Controllers/FileController.cs
+++ AsyncPushStreamContentExample/WebApplication/Controllers/FileController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace WebApplication.Controllers
@@ -15,16 +15,32 @@ namespace WebApplication.Controllers
         private static HttpClient Client { get; } = new HttpClient();
 
         // http://localhost:12084/api/file
-        public async Task<HttpResponseMessage> Get()
+        public HttpResponseMessage Get()
         {
-            var stream = await Client.GetStreamAsync("https://raw.githubusercontent.com/StephenClearyExamples/AsyncPushStreamContent/master/README.md");
+            var filenamesAndUrls = new Dictionary<string, string>
+            {
+                { "README.md", "https://raw.githubusercontent.com/StephenClearyExamples/AsyncPushStreamContent/master/README.md" },
+                { ".gitignore", "https://raw.githubusercontent.com/StephenClearyExamples/AsyncPushStreamContent/master/.gitignore" },
+            };
 
             var result = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                Content = new StreamContent(stream),
+                Content = new PushStreamContent(async (outputStream, httpContext, transportContext) =>
+                {
+                    using (var zipArchive = new ZipArchive(new WriteOnlyStreamWrapper(outputStream), ZipArchiveMode.Create))
+                    {
+                        foreach (var kvp in filenamesAndUrls)
+                        {
+                            var zipEntry = zipArchive.CreateEntry(kvp.Key);
+                            using (var zipStream = zipEntry.Open())
+                            using (var stream = await Client.GetStreamAsync(kvp.Value))
+                                await stream.CopyToAsync(zipStream);
+                        }
+                    }
+                }),
             };
-            result.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
-            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { FileName = "README.md" };
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { FileName = "MyZipfile.zip" };
             return result;
         }
     }
