dc60708b0fcc6faebaa3ee8526aa71aab1c821a7
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-10-15 14:15:10 -0400

    Basic stream download (of text file).

---
+++ AsyncPushStreamContentExample/src/WebApplication/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class FileController : Controller
+    {
+        private static HttpClient Client { get; } = new HttpClient();
+
+        // http://localhost:24425/api/file
+        [HttpGet]
+        public async Task<FileStreamResult> Get()
+        {
+            var stream = await Client.GetStreamAsync("https://raw.githubusercontent.com/StephenClearyExamples/AsyncPushStreamContent/master/README.md");
+
+            return new FileStreamResult(stream, new MediaTypeHeaderValue("text/plain"))
+            {
+                FileDownloadName = "README.md"
+            };
+        }
+    }
+}
