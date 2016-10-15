ab37b809e552be6d3cc84c62c6fb2fbc1e9cb563
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-10-15 15:19:16 -0400

    Pure core approach with ZipArchive.

--- AsyncPushStreamContentExample/src/WebApplication/Controllers/FileController.cs
+++ AsyncPushStreamContentExample/src/WebApplication/Controllers/FileController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -16,13 +17,29 @@ namespace WebApplication.Controllers
 
         // http://localhost:24425/api/file
         [HttpGet]
-        public async Task<FileStreamResult> Get()
+        public IActionResult Get()
         {
-            var stream = await Client.GetStreamAsync("https://raw.githubusercontent.com/StephenClearyExamples/AsyncPushStreamContent/master/README.md");
-
-            return new FileStreamResult(stream, new MediaTypeHeaderValue("text/plain"))
+            var filenamesAndUrls = new Dictionary<string, string>
             {
-                FileDownloadName = "README.md"
+                { "README.md", "https://raw.githubusercontent.com/StephenClearyExamples/AsyncPushStreamContent/master/README.md" },
+                { ".gitignore", "https://raw.githubusercontent.com/StephenClearyExamples/AsyncPushStreamContent/master/.gitignore" },
+            };
+
+            return new FileCallbackResult(new MediaTypeHeaderValue("application/octet-stream"), async (outputStream, _) =>
+            {
+                using (var zipArchive = new ZipArchive(new WriteOnlyStreamWrapper(outputStream), ZipArchiveMode.Create))
+                {
+                    foreach (var kvp in filenamesAndUrls)
+                    {
+                        var zipEntry = zipArchive.CreateEntry(kvp.Key);
+                        using (var zipStream = zipEntry.Open())
+                        using (var stream = await Client.GetStreamAsync(kvp.Value))
+                            await stream.CopyToAsync(zipStream);
+                    }
+                }
+            })
+            {
+                FileDownloadName = "MyZipfile.zip"
             };
         }
     }
---
+++ AsyncPushStreamContentExample/src/WebApplication/WriteOnlyStreamWrapper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+// This type is just here to work around a ZipArchive bug:
+//   https://connect.microsoft.com/VisualStudio/feedback/details/816411/ziparchive-shouldnt-read-the-position-of-non-seekable-streams (http://www.webcitation.org/6lGHjvc3C)
+//   https://github.com/dotnet/corefx/issues/11497
+
+namespace WebApplication
+{
+    public class WriteOnlyStreamWrapper : Stream
+    {
+        private readonly Stream _stream;
+        private long _position;
+
+        public WriteOnlyStreamWrapper(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public override bool CanRead => false;
+        public override bool CanSeek => false;
+        public override bool CanWrite => true;
+
+        public override long Position
+        {
+            get { return _position; }
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _position += count;
+            _stream.Write(buffer, offset, count);
+        }
+
+        public override void WriteByte(byte value)
+        {
+            _position += 1;
+            _stream.WriteByte(value);
+        }
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            _position += count;
+            return _stream.WriteAsync(buffer, offset, count, cancellationToken);
+        }
+
+
+
+        // Crap that we just have to forward.
+
+        public override bool CanTimeout => _stream.CanTimeout;
+        public override int ReadTimeout
+        {
+            get { return _stream.ReadTimeout; }
+            set { _stream.ReadTimeout = value; }
+        }
+        public override int WriteTimeout
+        {
+            get { return _stream.WriteTimeout; }
+            set { _stream.WriteTimeout = value; }
+        }
+
+        public override void Flush() => _stream.Flush();
+        public override Task FlushAsync(CancellationToken cancellationToken) => _stream.FlushAsync(cancellationToken);
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                _stream.Dispose();
+        }
+
+        public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
+        {
+            return _stream.CopyToAsync(destination, bufferSize, cancellationToken);
+        }
+
+
+        // Unsupported operations.
+
+        public override long Length
+        {
+            get
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
